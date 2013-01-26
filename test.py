5f466563f821824aeb7cc6a1060bd046fbbc4c9e
Mike Taylor <miket@opera.com>
2013-01-25 23:40:27 -0600

    Reorganize API finder tests; make them less boilerplatey.

--- test.py
+++ test.py
@@ -3,70 +3,65 @@
 import unittest
 from slimit.parser import Parser as JSParser
 from astwalker import *
+#TODO: test for false positives
 
 
-class TestUnaliased(unittest.TestCase):
+class TestAPIFinder(unittest.TestCase):
     def setUp(self):
         self.walker = ASTWalker()
+        self.jstree = JSParser()
+
+    def test_simple_find(self):
         script = """
-        var sendToKaleidos = opera.contexts.menu.createItem({
-          title: "Open image in Kaleidos",
-          contexts: ["image"],
-          icon: "butterfly_16.png",
-          onclick: function(event) {
-            opera.extension.tabs.create({
-              url: "http://coldhead.github.com/kaleidos/?src=" + event.srcURL,
-              focused: true
-            });
-          }
-        });
-        //add the menu item to the context menu
+        var sendToKaleidos = opera.contexts.menu.createItem(foo)
         opera.contexts.menu.addItem(sendToKaleidos);
         """
-        self.jstree = JSParser().parse(script)
-
-    def test_finder(self):
-        self.assertTrue(self.walker._find(self.jstree, 'addItem'))
+        self.assertTrue(self.walker._find(self.jstree.parse(script),
+                        'addItem'))
 
     def test_permission(self):
+        script = """
+        var sendToKaleidos = opera.contexts.menu.createItem(foo)
+        opera.contexts.menu.addItem(sendToKaleidos);
+        """
         self.assertEqual(self.walker.find_apicall(
-            self.jstree, 'addItem'), 'contextMenus')
+            self.jstree.parse(script), 'addItem'), 'contextMenus')
 
-
-class TestOneAlias(unittest.TestCase):
-    def setUp(self):
-        self.walker = ASTWalker()
+    def test_finder_aliased(self):
         script = """
         var mn = opera.contexts.menu;
         mn.addItem(button);
         """
-        self.jstree = JSParser().parse(script)
+        self.assertTrue(self.walker._find(self.jstree.parse(script),
+                        'addItem'))
 
-    def test_finder(self):
-        self.assertTrue(self.walker._find(self.jstree, 'addItem'))
-
-    def test_permission(self):
+    def test_permission_alised(self):
+        script = """
+        var mn = opera.contexts.menu;
+        mn.addItem(button);
+        """
         self.assertEqual(self.walker.find_apicall(
-            self.jstree, 'addItem'), 'contextMenus')
+            self.jstree.parse(script), 'addItem'), 'contextMenus')
 
-
-class TestMultiAlias(unittest.TestCase):
-    def setUp(self):
-        self.walker = ASTWalker()
+    def test_finder_multi_aliased(self):
         script = """
         var o = opera;
         c = o.contexts,
         mn = c.menu;
         mn.addItem(button);
         """
-        self.jstree = JSParser().parse(script)
+        self.assertTrue(self.walker._find(
+            self.jstree.parse(script), 'addItem'))
 
-    def test_finder(self):
-        self.assertTrue(self.walker._find(self.jstree, 'addItem'))
-
-    def test_permission(self):
+    def test_permission_multi_aliased(self):
+        script = """
+        var o = opera;
+        c = o.contexts,
+        mn = c.menu;
+        mn.addItem(button);
+        """
         self.assertEqual(self.walker.find_apicall(
-            self.jstree, 'addItem'), 'contextMenus')
+            self.jstree.parse(script), 'addItem'), 'contextMenus')
 
 
 class TestBrowserAction(unittest.TestCase):
@@ -105,7 +100,7 @@ class TestBrowserAction(unittest.TestCase):
         tb.addItem(lolwat)
         """
         self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
-        
+
     def test_aliased3(self):
         script = """
         var o = window.opera, c = o.contexts,
@@ -114,16 +109,8 @@ class TestBrowserAction(unittest.TestCase):
         self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
 
 
-def APIFinderSimple_suite():
-    return unittest.TestLoader().loadTestsFromTestCase(TestUnaliased)
-
-
-def APIFinderOneAlias_suite():
-    return unittest.TestLoader().loadTestsFromTestCase(TestOneAlias)
-
-
-def APIFinderMultiAlias_suite():
-    return unittest.TestLoader().loadTestsFromTestCase(TestMultiAlias)
+def APIFinder_suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TestAPIFinder)
 
 
 def browserAction_suite():
@@ -131,7 +118,5 @@ def browserAction_suite():
 
 if __name__ == '__main__':
         runner = unittest.TextTestRunner()
-        runner.run(APIFinderSimple_suite())
-        runner.run(APIFinderOneAlias_suite())
-        runner.run(APIFinderMultiAlias_suite())
+        runner.run(APIFinder_suite())
         runner.run(browserAction_suite())
