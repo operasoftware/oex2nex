c7f04c5891e216f02a2523b4af7f8341da97b8dd
Mike Taylor <miket@opera.com>
2013-01-25 23:27:10 -0600

    Add some tests for walker.find_button.

--- test.py
+++ test.py
@@ -69,6 +69,51 @@ class TestMultiAlias(unittest.TestCase):
             self.jstree, 'addItem'), 'contextMenus')
 
 
+class TestBrowserAction(unittest.TestCase):
+    def setUp(self):
+        self.walker = ASTWalker()
+        self.jstree = JSParser()
+
+    def test_unaliased(self):
+        script = """
+        opera.contexts.toolbar.addItem(lolwat)
+        """
+
+    def test_unaliased2(self):
+        script = """
+        window.opera.contexts.toolbar.addItem(lolwat)
+        """
+        self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
+
+    def test_unaliased3(self):
+        script = """
+        window["opera"].contexts.toolbar.addItem(lolwat)
+        """
+        self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
+
+    def test_aliased(self):
+        script = """
+        var ctx = window.opera.contexts;
+        ctx.toolbar.addItem(lolwat)
+        """
+        self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
+
+    def test_aliased2(self):
+        script = """
+        var ctx = window.opera.contexts;
+        tb = ctx.toolbar;
+        tb.addItem(lolwat)
+        """
+        self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
+        
+    def test_aliased3(self):
+        script = """
+        var o = window.opera, c = o.contexts,
+            t = c.toolbar, bloop = t.addItem(lolwat)
+        """
+        self.assertTrue(self.walker.find_button(self.jstree.parse(script)))
+
+
 def APIFinderSimple_suite():
     return unittest.TestLoader().loadTestsFromTestCase(TestUnaliased)
 
@@ -80,8 +125,13 @@ def APIFinderOneAlias_suite():
 def APIFinderMultiAlias_suite():
     return unittest.TestLoader().loadTestsFromTestCase(TestMultiAlias)
 
+
+def browserAction_suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TestBrowserAction)
+
 if __name__ == '__main__':
         runner = unittest.TextTestRunner()
         runner.run(APIFinderSimple_suite())
         runner.run(APIFinderOneAlias_suite())
         runner.run(APIFinderMultiAlias_suite())
+        runner.run(browserAction_suite())
