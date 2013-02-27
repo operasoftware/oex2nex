8c7cdd3afa3aae02e5f8d08b3a29816dec7ae289
Hari Kumar G <harig@opera.com>
2013-02-27 13:15:24 +0530

    Fix for too many parens added to function calls
    An earlier fix added a workaround for an SlimIt bug by adding parens.
    This along with the fact that textual replacement of 'textnew' was done
    all over the script text caused syntax error by creating code like
    
    function (foo()) { ... }
    
    Fixed this by applying the workaround to only specific cases where we
    want that to apply, i.e. code like -
    (function(){...}());

--- astwalker.py
+++ astwalker.py
@@ -130,9 +130,12 @@ class ASTWalker(NodeVisitor):
                             "text": fe, "textnew": fef}}]
                 if expr_root and isinstance(child, ast.FunctionCall):
                     fe = child.to_ecma()
-                    fef = '(' + fe + ')'
-                    yield [{"function-expr": {"scope": scope, "node": child,
-                        "text": fe, "textnew": fef}}]
+                    if re.match(r'^function[\n\s]*\([\n\s]*\)[\n\s]*{', fe ):
+                        if debug:
+                            print(">>>--- expr statement workaround:", fe)
+                        fef = '(' + fe + ')'
+                        yield [{"function-expr": {"scope": scope, "node": child,
+                                "text": fe, "textnew": fef}}]
                 # Descend
                 for subchild in self._get_replacements(child, aliases, scope + 1):
                     yield subchild
